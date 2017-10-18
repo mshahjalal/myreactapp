c1f3755deb9c1571d899dbf3561b4ae9700333e9
shahjalal <shahjalaldk@gmail.com>
2017-10-18 23:57:17 +0600

    add react router

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="Contact">
+          Contact content here...
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<BrowserRouter>
+		<div>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+				<li><Link to="/contact">Contact</Link></li>
+			</ul>
+		    <Route exact={true} path='/' component={Home} />
+		    <Route path='/about' component={About} />
+		    <Route path='/contact' component={Contact} />
+	    </div>
+	</BrowserRouter>, 
+	document.getElementById('root')
+);
