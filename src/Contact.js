5fbf1a9c1624b8d5680e23e29812375f62d8c150
shahjalal <shahjalaldk@gmail.com>
2017-10-19 11:25:04 +0600

    add form

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
-  render() {
-    return (
-      <div className="Contact">
-          Contact content here...
-      </div>
-    );
-  }
+		constructor(props) {
+	    super(props);
+	    this.state = {firstname: ''};
+
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		console.log('Change value is: ' + this.state.firstname);
+		this.setState({firstname: event.target.value});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		console.log('A name was submitted: ' + this.state.firstname);
+	}
+
+	
+	render() {
+		return (
+			<div className="Contact">
+				<form onSubmit={this.handleSubmit}>	
+					<label>
+						First Name
+						<input type="text" name="firstname" value={this.state.firstname} onChange={this.handleChange} placeholder="Your name.." />
+					</label>
+    				
+    				<input type="submit" value="Submit" />
+				</form>
+		  	</div>
+		);
+	}
 }
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
@@ -14,9 +14,14 @@ ReactDOM.render(
 				<li><Link to="/about">About</Link></li>
 				<li><Link to="/contact">Contact</Link></li>
 			</ul>
-		    <Route exact={true} path='/' component={Home} />
-		    <Route path='/about' component={About} />
-		    <Route path='/contact' component={Contact} />
+			<Switch>
+			    <Route exact={true} path='/' component={Home} />
+			    <Route path='/about' component={About} />
+			    <Route path='/contact' component={Contact} />
+			    <Route render={function(){
+			    	return <p>Not Found</p>
+			    }} />
+		    </Switch>
 	    </div>
 	</BrowserRouter>, 
 	document.getElementById('root')
